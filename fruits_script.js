5fd04a31e6d3bc259cfd24845e0baab17c2ae662
momonohasi <haiaka0126@icloud.com>
2026-05-10 00:31:51 +0900

    Update fruits_script.js

--- fruits_script.js
+++ fruits_script.js
@@ -1,9 +1,32 @@
+const hour = 1; // ←ここ変えると時間帯テストできる！
+
+if (hour >= 0 && hour < 6) {
+  // 朝
+  document.body.style.background =
+    "linear-gradient(135deg, #e8ffe3, #d6fff5, #dff4ff)";
+} 
+else if (hour >= 6 && hour < 12) {
+  // 昼
+  document.body.style.background =
+    "linear-gradient(135deg, #fff4d6, #ffdff0, #e6fff8)";
+} 
+else if (hour >= 12 && hour < 18) {
+  // 夕方
+  document.body.style.background =
+    "linear-gradient(135deg, #ffe0c9, #ffd6f5, #e7dcff)";
+} 
+else {
+  // 夜
+  document.body.style.background =
+    "linear-gradient(135deg, #dff4ff, #e4dcff, #f3e1ff)";
+}
+
 const cards = document.querySelectorAll(".fruit-card");
 
 cards.forEach((card) => {
   card.addEventListener("mouseenter", () => {
     const emoji = card.dataset.emoji;
-    const count = Math.floor(Math.random() * 9) + 8; // 8〜16個
+    const count = Math.floor(Math.random() * 9) + 8;
 
     for (let i = 0; i < count; i++) {
       const pop = document.createElement("span");
